551fba13ef0e0ca0fe69e116b6cde6e70efcd8fd
Ibukun <ibukunolatunde1@gmail.com>
2022-02-14 10:47:44 -0600

    Added Onica Project

---
+++ 13-web-linux-vmss-resource.tf
@@ -0,0 +1,61 @@
+# Locals Block for custom data
+locals {
+webvm_custom_data = <<CUSTOM_DATA
+#!/bin/sh
+#sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl enable httpd
+sudo systemctl start httpd  
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+sudo chmod -R 777 /var/www/html 
+sudo echo "Hello World - VM Hostname: $(hostname)" > /var/www/html/index.html
+sudo curl -H "Metadata:true" --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2020-09-01" -o /var/www/html/app1/metadata.html
+CUSTOM_DATA  
+}
+
+
+# Resource: Azure Linux Virtual Machine Scale Set - App1
+resource "azurerm_linux_virtual_machine_scale_set" "web_vmss" {
+  name                = "${local.resource_name_prefix}-web-vmss"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard_DS1_v2"
+  instances           = 2
+  admin_username      = "azureuser"
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("${path.module}/ssh-keys/terraform-azure.pub")
+  }
+
+  source_image_reference {
+    publisher = "RedHat"
+    offer = "RHEL"
+    sku = "83-gen2"
+    version = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  upgrade_mode = "Automatic"
+  
+  network_interface {
+    name    = "web-vmss-nic"
+    primary = true
+    network_security_group_id = azurerm_network_security_group.web_vmss_nsg.id
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = azurerm_subnet.websubnet.id  
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.web_lb_backend_address_pool.id]
+    }
+  }
+  #custom_data = filebase64("${path.module}/app-scripts/redhat-app1-script.sh")      
+  custom_data = base64encode(local.webvm_custom_data)  
+}
+  
+
---
+++ app-scripts/redhat-webvm-script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl enable httpd
+sudo systemctl start httpd  
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+sudo chmod -R 777 /var/www/html 
+sudo echo "Hello World - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/index.html
+sudo curl -H "Metadata:true" --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2020-09-01" -o /var/www/html/app1/metadata.html
